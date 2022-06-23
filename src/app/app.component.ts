9871ac2a62fc09290ac132d82fabd3b94ec49169
paulo <ronaldotpmilfont@gmail.com>
2022-06-23 13:21:13 -0300

    refazendo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,3 +10,35 @@ export class AppComponent {
   
   
 }
+import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+@Output() notify = new EventEmitter();
+
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+  ProductDetailsComponent,
+  product: Product | undefined;
+  /* ... */
+  constructor(private route: ActivatedRoute) { }
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product!: Product;
+  this.product = products.find(product => product.id === productIdFromRoute);
+}
+  constructor() { }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
+  
+
+  ngOnInit() {
+  }
+
+}
